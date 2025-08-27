ffb0ef960d0c288e33cb9cc3879e84b167b6160f
Eriec Tanijaya <eriec@grip-principle.com>
2025-08-27 18:05:56 +0700

    feat: basic layout

--- packages/frontend/src/app.tsx
+++ packages/frontend/src/app.tsx
@@ -1,3 +1,9 @@
 export function App() {
-	return <div className="font-mono">wasuupp!!</div>;
+	return (
+		<div className="flex flex-col min-h-screen">
+			<header className="bg-gray-400 p-4">Header</header>
+			<main className="p-4 grow">Content</main>
+			<footer className="bg-gray-400 p-4">Footer</footer>
+		</div>
+	);
 }
