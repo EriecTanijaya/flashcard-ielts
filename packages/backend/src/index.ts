1879bfabff21107b6a4fafed3f80b54cb9a83fd7
Eriec Tanijaya <eriec@grip-principle.com>
2025-09-19 19:52:56 +0700

    feat: show the local area network address to easier debugging on phone

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,8 +1,9 @@
-import { serve } from "bun";
+import { type Server, serve } from "bun";
+import os from "os";
 import dashboard from "../../frontend/public/dashboard.html";
 
 export async function bootstrapBackend() {
-	serve({
+	const server = serve({
 		routes: {
 			"/": dashboard,
 			"/api/questions": {
@@ -28,4 +29,30 @@ export async function bootstrapBackend() {
 			hmr: true,
 		},
 	});
+
+	console.log(`Listening to ${getNetworkAddress(server)}`);
+}
+
+function getNetworkAddress(server: Server): string | null {
+	if (process.env.NODE_ENV === "production") {
+		return server.url.origin;
+	}
+
+	const networkInterfaces = os.networkInterfaces();
+
+	for (const interfaceName in networkInterfaces) {
+		const interfaces = networkInterfaces[interfaceName];
+
+		if (!interfaces) {
+			continue;
+		}
+
+		for (const iface of interfaces) {
+			if (iface.family === "IPv4" && !iface.internal) {
+				return `http://${iface.address}:${server.port}`;
+			}
+		}
+	}
+
+	return null; // Return null if no suitable interface is found
 }
